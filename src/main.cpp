270eaa64c067d7320a80eaf435ca7255dd881426
Patrick Servello <patrick.servello@gmail.com>
2019-10-02 07:56:57 -0500

    Created example main.cpp file

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "httplib.h"
+
+int main() {
+  std::cout << "Main launched!!" << std::endl;
+  std::cout << "Using external resource httplib to make a get request..." << std::endl;
+  
+  httplib::Client cli("postman-echo.com", 80);
+
+  auto res = cli.Get("/get");
+  if (res && res->status == 200) {
+      std::cout << res->body << std::endl;
+  }
+  
+  
+  return 0;
+}
